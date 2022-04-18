0d291ce56654a68ca8b5c484d430bea649af49cc
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-18 14:09:25 -0700

    4-53 listening to user input and get user input by add titleChangeHandler in ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 const ExpenseForm = () => {
+  //document.getElementById('').addEventListener('click',(event)=>{}) //vanila javascript return event object from click
+  //same as above way to get event, below handler also get event, just add event parameter inside of titleChangeHandler = (event)
+  const titleChangeHandler = (event) => {
+    //console.log(event); // will get a lot of date for event, there is a field "input " which will simply
+    //points at thte DOM element for which the events occured
+    // the value property holds the current value of this input
+    console.log(event.target.value); // This is the way to get the value!
+  }; // get event when event occurs at the point of time this event occurs
+
   return (
     <form>
       <div className="  new-expense_controls">
         <div className="new-expense_control ">
           <label> Title </label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense_control ">
@@ -28,3 +37,7 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+//onInput for key strock
+//onChange for key strock for same events of all input type
+//  <input type="text" onChange={ titleChangeHandler} />  here passed pointer
+//vanila javascript( js without react )
