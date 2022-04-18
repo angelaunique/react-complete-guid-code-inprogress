5c7eadf9bc4dbf5093d283485071830e8982555c
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-18 15:04:07 -0700

    group three states into one state object and use spread opeartor for handlers

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 
 const ExpenseForm = () => {
-  //document.getElementById('').addEventListener('click',(event)=>{}) //vanila javascript return event object from click
-  //same as above way to get event, below handler also get event, just add event parameter inside of titleChangeHandler = (event)
-  const [enteredTitle, setEnteredTilte] = useState(""); //initally when the component rendered for the first time, nothing was entered, so empty string for para
-  // the function to update the state is setEnteredTilte
-
-  const [enteredAmout, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmout: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    /*console.log(event); */ // get event when event occurs at the point of time this event occurs
-    // will get a lot of date for event, there is a field "input " which will simply
-    //points at thte DOM element for which the events occured
-    // the value property holds the current value of this input
-    /* console.log(event.target.value); // This is the way to get the value! */
-    // to store the input value, when form is submit, so we can use that value
-    // one way to make sure the input survives even after the component "return " is re excuted is to useSate
-    setEnteredTilte(event.target.value); // need to save this to somewhere that ditach it from development cycle
+    setUserInput({
+      ...userInput, //by using spread operator we ensure other part of the states are not throw away but it's part of new state
+      enteredTitle: event.target.value,
+    });
+  };
 
-    const amountChangeHandler = (event) => {
-      setEnteredAmount(event.target.value);
-    };
-    const dateChangeHandler = (event) => {
-      setEnteredDate(event.target.value);
-    };
+  const amountChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredAmout: event.target.value,
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   return (
