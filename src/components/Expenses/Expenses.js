a9b7b17ec47074c8d54f4e3f120c59259ea9a660
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-18 22:08:12 -0700

    src/components/Expenses/Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,32 +17,22 @@ const Expenses = (props) => {
   };
 
   // expense is control component which control ExpensesFilter component
+  //dynamic expression, transform expense obj  js elements in {[]}
+  // use map to extract title, amount,date from props.items array, transform array to an array full of JSX item
   return (
     <div>
       <Card className="expenses">
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
-        />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
         />
       </Card>
     </div>
@@ -50,7 +40,4 @@ const Expenses = (props) => {
 };
 
 export default Expenses;
-//Card 变化是最外圈的框从方角变成圆角
-
-// inside the root <div> there can be multiple
-// expense filter is inside of the vcard , thus add a div to outside of Card
+// expense is used to extract tile:    {props.items.map(expense => <ExpenseItem title = {expense.title}/>)}
