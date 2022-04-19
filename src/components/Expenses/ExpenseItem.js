848dad44ee4c5182395dbf005e9d5985c6f032c7
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-18 18:48:48 -0700

    add expensefilter component and add it to inside of Card

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,24 +2,15 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import React, { useState } from "react"; // useState allow to to define variable as state and change as value change
+import ExpensesFilter from "./ExpenseFilter";
 //function ExpenseItem(title,amount,date) {
 //function ExpenseItem(props) {
 const ExpenseItem = (props) => {
-  //let title = props.title;
-  //why const while still change? not use = ,  but use the function, the const value is managed by react,
-  // never assign new value = title, how to we get latest value of title
-  // whenever component return""" excute, thus the new title is fectched from react
-  // when re extute, bc state change, react will not reinitialize the state, will detect react initialzed in the past
-  //and would just grap the latest state.
-  const [title, setTitle] = useState(props.title); // must be called directly in component functions, not outside nor inside nested functions clickHandler()
-  //useState return a new title and an updated function(the function that updated the value),always return arr with 2 values,
-  // title is current value and setTitle is the function that updated it, react keep track of useState when called the first time
-
-  console.log("ExpreseItem evaluated by react"); //will be called 4 times everytime expenseItem is called
+  const [title, setTitle] = useState(props.title);
+  // console.log("ExpreseItem evaluated by react"); //will be called 4 times everytime expenseItem is called
   const clickHandler = () => {
     setTitle("Upldated!");
-    console.log(title); // change item, then reevaluate in return, by default,regular varialbes like let tile = are not triggering reevaluation, thus no changed show ,bc the overall component function doesnt excute again just bc some variable changes
-    //title = "Updated!@@"; // to tell the react it should exculte, should import named import from react library
+    console.log(title);
   };
 
   return (
@@ -37,4 +28,3 @@ export default ExpenseItem;
 //component are functions that return html code； 注意！ comment不能放第一行！！
 // only one root element in one statement : return <h2>Expense Item!</h2>;
 //Sol: wrap into different pair of divs
-// inside the root <div> there can be multiple
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,45 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import "./Expenses.css";
 //function Expenses(props) {
+//every item reach it's own states, this func called 4 times ,every call, a new seperate state is created
+// if change title on one of it, others won't change, on per component basis
+
+// put <ExpensesFilter /> inside of Card but before   <ExpenseItem
 const Expenses = (props) => {
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card> //变化是最外圈的框从方角变成圆角
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 
 export default Expenses;
+//Card 变化是最外圈的框从方角变成圆角
+
+// inside the root <div> there can be multiple
+// expense filter is inside of the vcard , thus add a div to outside of Card
